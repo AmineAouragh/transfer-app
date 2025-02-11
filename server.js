ade61c46001f5fe2335758bee38c1862a1c3eb9a
Amine Aouragh <amine.aouragh.20@gmail.com>
2025-02-11 18:56:16 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const path = require('path')
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type']
+}))
 
 const PORT = 8080
 
